e878dc4b50b1f0d907fb77f7a4324cd1d2c8dcdb
samyas <abdessalemsamet@gmail.com>
2019-04-12 01:17:56 +0200

    ci

---
+++ build_scripts/deploy_config.sh
@@ -0,0 +1,11 @@
+    
+# !/bin/bash
+# deploy_config.sh
+# This script updates Environment Variables at deployment
+# Note that sed on OSX and sed in Linux operate different ways
+# with OSX you have to pass a dummy blank file with -i like 'sed -i "" <s command> <file>'
+
+cd src/environments
+sed -i 's/\(OPEN_PPMS_API_KEY\)/'$OPEN_PPMS_API_KEY'/' environment.prod.ts
+# console log the file to verify that the values were set
+cat environment.prod.ts
